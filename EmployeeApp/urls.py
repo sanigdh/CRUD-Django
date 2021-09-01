3b780910fb1cb1528ae0e89b91fb914b44c263d2
Sanigdh Chandan <sanigdh@Sanigdhs-MacBook-Air.local>
2021-09-01 22:37:14 +0530

    added employee module

--- EmployeeApp/urls.py
+++ EmployeeApp/urls.py
@@ -3,5 +3,7 @@ from EmployeeApp import views
 
 urlpatterns = [
     url(r'^department$',views.departmentApi),
-    url(r'^department/([0-9]+)$',views.departmentApi)
+    url(r'^department/([0-9]+)$',views.departmentApi),
+    url(r'^employee$',views.employeeApi),
+    url(r'^employee/([0-9]+)$',views.employeeApi),
 ]
--- EmployeeApp/views.py
+++ EmployeeApp/views.py
@@ -36,3 +36,36 @@ def departmentApi(request,id=0):
         department=Department.objects.get(DepartmentId=id)
         department.delete()
         return JsonResponse("Department Deleted Successfully",safe=False)
+
+@csrf_exempt
+def employeeApi(request,id=0):
+    if request.method=='GET':
+        employees = Employees.objects.all()
+        employees_serializer=EmployeeSerializers(employees,many=True)
+        return JsonResponse(employees_serializer.data,safe=False)
+    elif request.method=='POST':
+        employee_data=JSONParser().parse(request)
+        employees_serializer=EmployeeSerializers(data=employee_data)
+        if employees_serializer.is_valid():
+            employees_serializer.save()
+            return JsonResponse("Added Successfully",safe=False)
+        return JsonResponse(employees_serializer.errors)
+        return JsonResponse("Failed to Add",safe=False)
+    elif request.method=='PUT':
+        employee_data=JSONParser().parse(request)
+        employee=Employees.objects.get(EmployeeId=employee_data['EmployeeId'])
+        employees_serializer=EmployeeSerializers(employee,data=employee_data)
+        if employees_serializer.is_valid():
+            employees_serializer.save()
+            return JsonResponse("Updated Successfully",safe=False)
+        return JsonResponse("Failed to Update")
+    elif request.method=='DELETE':
+        employee=Employees.objects.get(EmployeeId=id)
+        employee.delete()
+        return JsonResponse("Deleted Successfully",safe=False)
+
+@csrf_exempt
+def SaveFile(request):
+    file=request.FILES['file']
+    file_name=default_storage.save(file.name,file)
+    return JsonResponse(file_name,safe=False)
